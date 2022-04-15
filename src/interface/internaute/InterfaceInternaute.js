c869709d833a2acfe2962649a664fe36ae72aad1
Feryel Ben Chaabene <benchaabeneferyel@gmail.com>
2022-04-15 16:25:15 +0100

    15/04/2022

---
+++ src/interface/internaute/InterfaceInternaute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import InterHeader from './InterHeader';
+import Interfooter from './InterFooter';
+import InterMain from './InterMain';
+
+
+export default function InterfaceInternaute() {
+  return (
+    <>
+      <InterHeader/> 
+      <InterMain/>
+      <Outlet/>
+      <Interfooter/>
+    </>  )
+}
